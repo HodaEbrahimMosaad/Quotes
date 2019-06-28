425c48f1274e4981dcc1b80b00095dbdbc62e572
Hoda Ibrhim <you@example.com>
2019-06-28 05:43:56 +0200

    Final Product

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <new-quote @addNewQuote="newQuote"></new-quote>
+        <quote-grid :quotes="quotes" @delQuote="dropQuote"></quote-grid>
+    </div>
+</template>
+
+<script>
+    import QouteGrid from "./components/QoutesGrid.vue";
+    import NewQoute from "./components/NewQoute.vue";
+    import Header from "./components/Headr.vue";
+    export default {
+        data(){
+          return {
+              quotes: ['test', 'test2'],
+              maxQuotes: 10
+          }
+        },
+        components: {
+            "quote-grid": QouteGrid,
+            "new-quote": NewQoute,
+            "app-header": Header,
+        },
+        methods:{
+            newQuote(quote){
+                if(this.quotes.length >= 10){
+                    return alert('Delete some quotes, maximum is :' + this.maxQuotes);
+                }
+                this.quotes.push(quote)
+            },
+            dropQuote(index){
+                this.quotes.splice(this.quotes.indexOf(index),1);
+            }
+        },
+
+
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QoutesGrid.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <app-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">{{ quote }}</app-quote>
+    </div>
+</template>
+<script>
+    import Qoute from "./Qoute.vue";
+    export default {
+        props: ['quotes'],
+        components: {
+            "app-quote": Qoute
+        },
+        methods: {
+            deleteQuote(index){
+                this.$emit('delQuote', index)
+            }
+        }
+    }
+</script>
